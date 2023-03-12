51ccbdae90985676288d65d4364484007103165a
Mohammed Amarnah <m.amarnah@gmail.com>
2023-03-11 22:16:34 -0600

    Add tailwind support (#2)

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text className='font-bold text-gray-500 text-4xl'>Hello, World</Text>
     </View>
   );
 }
